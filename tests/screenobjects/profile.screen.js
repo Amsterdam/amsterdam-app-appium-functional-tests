f2e92dd720e90048f989e7583133d25e235ce88d
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-06-29 09:41:44 +0200

    add addAddress + checkAddressAdded function

--- tests/screenobjects/profile.screen.js
+++ tests/screenobjects/profile.screen.js
@@ -1,4 +1,4 @@
-import createSelector from '../Shared/helpers/helpers.js';
+import helpers from '../Shared/helpers/helpers.js';
 import Screen from './screen.js';
 /**
  * sub page containing specific selectors and methods for a specific page
@@ -6,35 +6,78 @@ import Screen from './screen.js';
 class ProfileScreen extends Screen {
 
     get addressTitle() {
-        return createSelector("AddressTitle");
+        return helpers.createSelector("AddressTitle");
     }
 
     get addressInstructionParagraph() {
-        return createSelector("AddressInstructionParagraph")
+        return helpers.createSelector("AddressInstructionParagraph")
     }
 
     get addressAddButton() {
-        return createSelector("AddressAddButton")
+        return helpers.createSelector("AddressAddButton")
     }
 
     get addressStreetInputSearchField() {
-        return createSelector("AddressStreetInputSearchField")
+        return helpers.createSelector("AddressStreetInputSearchField")
     }
 
     get addressSearchResultWeesperstraat113() {
-        return createSelector("AddressSearchResultWeesperstraat 113Button")
+        return helpers.createSelector("AddressSearchResultWeesperstraat 113Button")
     }
 
     get addressSearchResultBalistraat1hg1() {
-        return createSelector("AddressSearchResultBalistraat 1-1Button")
+        return helpers.createSelector("AddressSearchResultBalistraat 1-1Button")
     }
 
     get addressSearchResultWeesperstraat() {
-        return createSelector("AddressSearchResultWeesperstraatButton")
+        return helpers.createSelector("AddressSearchResultWeesperstraatButton")
     }
 
+    get addressTooltipButton() {
+        return helpers.createSelector("AddressTooltipButton")
+    }
+
+    get addressEditButton() {
+        return helpers.createSelector("AddressEditButton")
+    }
+
+    get addressDeleteButton() {
+        return helpers.createSelector("AddressDeleteButton")
+    }
+
+    get addressStreetnameAndNumberText() {
+        return helpers.createSelector("AddressStreetnameAndNumberText")
+    }
+
+    get addressPostalcodeAndCityText() {
+        return helpers.createSelector("AddressPostalcodeAndCityText")
+    }
+
+    get deletedTxt() {
+        return "~Gelukt, Het adres is verwijderd uit uw profiel."
+    }
+
+    // get deletedTxt() {
+    //     return createContentSelector("~Gelukt, Het adres is verwijderd uit uw profiel.")
+    // }
+
     async addressSelector(adres) {
-        return createSelector(`AddressSearchResult${adres}Button`)
+        return helpers.createSelector(`AddressSearchResult${adres}Button`)
+    }
+
+    async addAddress(adres) {
+        await expect(this.headerTitle).toHaveText('Adres')
+        await this.addressStreetInputSearchField.waitForDisplayed(3000)
+        await driver.pause(2000)
+        await this.addressStreetInputSearchField.addValue(adres)
+    }
+
+    async checkAddressAdded(adres, postcode) {
+        await expect(this.addressTitle).toBeDisplayed()
+        await expect(this.addressStreetnameAndNumberText).toBeDisplayed()
+        await expect(this.addressPostalcodeAndCityText).toBeDisplayed()
+        await expect(this.addressStreetnameAndNumberText).toHaveText(adres)
+        await expect(this.addressPostalcodeAndCityText).toHaveText(postcode)
     }
 }
 
