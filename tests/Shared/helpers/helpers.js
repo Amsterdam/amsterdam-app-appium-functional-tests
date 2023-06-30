31e393687a1d81e12f8cd1225cb06489407b54b9
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-06-30 09:56:28 +0200

    new helper function for creating selector with content-des

--- tests/Shared/helpers/helpers.js
+++ tests/Shared/helpers/helpers.js
@@ -1,12 +1,21 @@
-export default function createSelector(id) {
-  const currentOS = driver.capabilities.platformName
-  const testID = currentOS === 'iOS' ? `~${id}` : `android=new UiSelector().resourceId("${id}")`
-  return $(testID)
-}
+class Helpers {
 
+  createSelector(id) {
+    const currentOS = driver.capabilities.platformName
+    const testID = currentOS === 'iOS' ? `~${id}` : `android=new UiSelector().resourceId("${id}")`
+    return $(testID)
+  }
 
+  createContentSelector(id) {
+    const currentOS = driver.capabilities.platformName
+    const testID = currentOS === 'iOS' ? `~${id}` : `android=new UiSelector().descriptionMatches("${id}")`
+    return $(testID)
+  }
   // export default function checkOS() {
   //   const currentOS = driver.capabilities.platformName
   //   return currentOS
   // }
 
+}
+
+export default new Helpers;
