f60091829de4d8f28e0b940e51cf06b182f220bd
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-07-26 11:11:50 +0200

    add select random title and text function

--- tests/features/functional/testdata/pushberichten.js
+++ tests/features/functional/testdata/pushberichten.js
@@ -1,27 +1,46 @@
-const titels = [
-    "Groene oase in Amstel III: Gemeenschappelijke moestuinen voor bewoners",
-    "Duurzaam pendelen: Amstel III introduceert fietsdeelprogramma met elektrische fietsen",
-    "Elektrisch delen: Autodeelplatform met elektrische auto's gelanceerd in Amstel III",
-    "Zen in de natuur: Wekelijkse yoga- en meditatiesessies in het groene park van Amstel III",
-    "Lokale kunstenaars verfraaien muren met prachtige muurschilderingen in Amstel III",
-    "Energietransitie in Amstel III: Programma voor energiezuinige woningen gestart",
-    "Lokaal en vers: Boerenmarkt in Amstel III verbindt bewoners met lokale producenten",
-    "Duurzaam watergebruik: Amstel III moedigt bewoners aan om regenwater op te vangen",
-    "Koken met impact: Gratis workshops over duurzaam koken en voedselverspilling in Amstel III",
-    "Composteerrevolutie: Buurtcomposteerprogramma in Amstel III voor groenere tuinen"
-];
-
-const tekst = [
-    "In Amstel III kunnen bewoners binnenkort genieten van gemeenschappelijke moestuinen. Vers groenten en kruiden verbouwen wordt eenvoudig en duurzaam.",
-    "Amstel III maakt groen woon-werkverkeer mogelijk met een nieuw fietsdeelprogramma. Elektrische fietsen zijn beschikbaar voor bewoners, voor een duurzaam en gezond transport.",
-    "Amstel III zet in op duurzame mobiliteit met een autodeelplatform vol elektrische auto's. Bewoners kunnen gemakkelijk auto's delen en zo bijdragen aan een groenere omgeving.",
-    "Ontspan en kom tot rust tijdens de wekelijkse yoga- en meditatiesessies in het groene park van Amstel III. Geniet van de natuurlijke omgeving en verbeter je welzijn.",
-    "Amstel III krijgt een kleurrijke make-over door lokale kunstenaars. Saaie muren worden omgetoverd tot prachtige muurschilderingen, waardoor de wijk tot leven komt.",
-    "Amstel III biedt bewoners subsidies en advies om hun huizen energiezuiniger te maken. Een stap richting duurzaam wonen en een groenere toekomst.",
-    "Kom naar de boerenmarkt in Amstel III en geniet van verse, biologische producten rechtstreeks van lokale boeren. Een duurzame en smakelijke keuze.",
-    "Leer duurzaam koken en ontdek hoe je voedselverspilling kunt verminderen tijdens de gratis workshops in Amstel III. Een smakelijke en milieubewuste ervaring.",
-    "Amstel III lanceert een revolutionair buurtcomposteerprogramma. Bewoners kunnen organisch afval omzetten in vruchtbare compost voor groenere tuinen en een duurzamere leefomgeving."
+const data = [{
+    title: "Groene oase in Amstel III",
+    text: "In Amstel III kunnen bewoners binnenkort genieten van gemeenschappelijke moestuinen. Vers groenten en kruiden verbouwen wordt eenvoudig en duurzaam."
+},
+{
+    title: "Duurzaam pendelen",
+    text: "Amstel III maakt groen woon-werkverkeer mogelijk met een nieuw fietsdeelprogramma. Elektrische fietsen zijn beschikbaar voor bewoners, voor een duurzaam en gezond transport."
+},
+{
+    title: "Elektrisch delen",
+    text: "Amstel III zet in op duurzame mobiliteit met een autodeelplatform vol elektrische auto's. Bewoners kunnen gemakkelijk auto's delen en zo bijdragen aan een groenere omgeving."
+},
+{
+    title: "Zen in de natuur",
+    text: "Ontspan en kom tot rust tijdens de wekelijkse yoga- en meditatiesessies in het groene park van Amstel III. Geniet van de natuurlijke omgeving en verbeter je welzijn."
+},
+{
+    title: "Lokale kunstenaars",
+    text: "Amstel III krijgt een kleurrijke make-over door lokale kunstenaars. Saaie muren worden omgetoverd tot prachtige muurschilderingen, waardoor de wijk tot leven komt."
+},
+{
+    title: "Energietransitie in Amstel III",
+    text: "Amstel III biedt bewoners subsidies en advies om hun huizen energiezuiniger te maken. Een stap richting duurzaam wonen en een groenere toekomst."
+},
+{
+    title: "Lokaal en vers",
+    text: "Kom naar de boerenmarkt in Amstel III en geniet van verse, biologische producten rechtstreeks van lokale boeren. Een duurzame en smakelijke keuze."
+},
+{
+    title: "Duurzaam watergebruik",
+    text: "Amstel III stimuleert waterbesparing en vergroening door bewoners aan te moedigen regenwater op te vangen met regentonnen. Een kleine stap met een groot verschil."
+},
+{
+    title: "Koken met impact",
+    text: "Leer duurzaam koken en ontdek hoe je voedselverspilling kunt verminderen tijdens de gratis workshops in Amstel III. Een smakelijke en milieubewuste ervaring."
+},
+{
+    title: "Composteerrevolutie",
+    text: "Amstel III lanceert een revolutionair buurtcomposteerprogramma. Bewoners kunnen organisch afval omzetten in vruchtbare compost voor groenere tuinen en een duurzamere leefomgeving."
+},
 ]
 
-export const randomtitel = titels[Math.floor(Math.random() * titels.length)];
-export const randomtekst = tekst[Math.floor(Math.random() * tekst.length)];
+export const randomTitleText = () => {
+    return data[randInt()]
+}
+export const randInt = () => { return Math.floor(Math.random() * data.length) }
