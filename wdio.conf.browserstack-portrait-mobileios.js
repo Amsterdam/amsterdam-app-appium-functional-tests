5f087e56f27ef1151a0093ddc85288baf17f74a0
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-10-18 16:51:47 +0200

    add configs for more devices

---
+++ wdio.conf.browserstack-portrait-mobileios.js
@@ -0,0 +1,186 @@
+import { bsUrliOS } from './credentials.js';
+import { config as sharedconfig } from "./wdio.conf.shared.js";
+
+sharedconfig.capabilities = [
+
+    {
+        platformName: "ios",
+        "appium:platformVersion": "12",
+        "appium:deviceName": "iPhone 7",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        //"appium:fullReset": "true",
+        //"appium:autoAcceptAlerts": "true",
+        //"appium:clearPackageData": true,
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+            //"autoAcceptAlerts": "true"
+            //"deviceOrientation": "landscape",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "13",
+        "appium:deviceName": "iPhone 8",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "14",
+        "appium:deviceName": "iPhone 12",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "15",
+        "appium:deviceName": "iPhone XS",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "15",
+        "appium:deviceName": "iPhone 11",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "16",
+        "appium:deviceName": "iPhone SE 2020",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "16",
+        "appium:deviceName": "iPhone 12 Mini",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "16",
+        "appium:deviceName": "iPhone 14 Plus",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "17",
+        "appium:deviceName": "iPhone 13",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+    {
+        platformName: "ios",
+        "appium:platformVersion": "17",
+        "appium:deviceName": "iPhone 15 Pro Max",
+        "appium:app": bsUrliOS,
+        "appium:noReset": "false",
+        'bstack:options': {
+            "projectName": "amsterdam-app-mobile-ios",
+            "buildName": "Sprint 42 - iOS - run 3",
+            "appiumVersion": "2.0.1",
+            "midSessionInstallApps": [bsUrliOS],
+            "enableCameraImageInjection": "true",
+        },
+    },
+
+]
+
+sharedconfig.services = [
+    ['browserstack', {
+        browserstackLocal: false
+    }]
+]
+
+// Patterns to exclude.
+sharedconfig.exclude = [
+    //"./tests/features/functional/settings.feature",
+    "./tests/features/functional/notifications_simulator.feature",
+    //"./tests/features/functional/mijnprofiel.feature",
+    //"./tests/features/functional/notifications.feature",
+    //"./tests/features/functional/launchapp.feature",
+    //"./tests/features/functional/construction-work.feature",
+    //"./tests/features/functional/contact.feature",
+    "./tests/features/visual-eyes/about.feature",
+    "./tests/features/visual-eyes/construction-work.feature",
+    "./tests/features/visual-eyes/contact.feature",
+    "./tests/features/visual-eyes/open-waste-container.feature",
+    "./tests/features/visual-eyes/redirects.feature",
+    "./tests/features/visual-eyes/report-problem.feature",
+    "./tests/features/visual-eyes/waste-guide.feature",
+    "./tests/features/visual-percy/about.feature",
+    "./tests/features/visual-percy/construction-work.feature",
+    "./tests/features/visual-percy/contact.feature",
+    "./tests/features/visual-percy/redirects.feature",
+    "./tests/features/visual-percy/report-problem.feature",
+    "./tests/features/visual-percy/waste-guide.feature",
+    "./tests/features/location-features/my-location-construction-work-android.feature",
+    "./tests/features/location-features/my-location-waste-guide-android.feature"
+]
+
+export const config = sharedconfig;
