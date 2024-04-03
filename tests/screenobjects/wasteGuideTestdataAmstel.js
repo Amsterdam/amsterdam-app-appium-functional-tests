9923d7c04c29f5d55f96672ff082fbf87fa6d16c
Monique van Benthem <m.van.benthem@amsterdam.nl>
2024-04-03 10:38:37 +0200

    add testdata

---
+++ tests/screenobjects/wasteGuideTestdataAmstel.js
@@ -0,0 +1,28 @@
+export const wasteGuideAmstel = {
+    iOS: {
+        wasteGuideReportWrongBuildingTypeIntroPhrase: "Dit is geen woonadres",
+        wasteGuideReportWrongBuildingTypePhrase: "Klopt dit niet?",
+        wasteGuideReportWrongBuildingTypeLink: "Geeft het door",
+        wasteGuideSelectContractRadioGroupfalseRadioButton: "Ik heb maximaal 9 zakken afval per week en betaal reinigingsrecht.",
+        wasteGuideSelectContractRadioGrouptrueRadioButton: "Ik heb een contract voor mijn afval.",
+        wasteGuideBusinessesInfoTitle: "Neem contact op met uw afvalinzamelaar",
+        wasteGuideBusinessesInfoPhrase: "Of kijk voor meer informatie over bedrijfsafval op",
+        wasteGuideBusinessesLink: "onze website."
+    },
+    Android: {
+        wasteGuideRestafvalHowSection: "Hoe: In de container voor restafval",
+        wasteGuideRestafvalWhereSection: "Waar: Kaart met containers in de buurt",
+        wasteGuideGrofAfvalCollectionPointsLink: "Kaart met recyclepunten in de buurt",
+        wasteGuideGrofAfvalDaySection: "Ophaaldag: Maandag en vrijdag", wasteGuideGrofAfvalOutsideSection: "Buitenzetten: Zondag vanaf 21.00 tot maandag 07.00 uur en donderdag vanaf 21.00 tot vrijdag 07.00 uur",
+        wasteGuideGrofAfvalWhereSection: "Waar: Aan de rand van de stoep of op de vaste plek",
+        wasteGuidePapierEnKartonHowSection: "Hoe: In de container voor papier",
+        wasteGuidePapierEnKartonWhereSection: "Waar: Kaart met containers in de buurt",
+        wasteGuideGroenteFruitEtensrestenEnTuinafvalGfeTHowSection: "Hoe: Bij het restafval",
+        wasteGuideGroenteFruitEtensrestenEnTuinafvalGfeTWhereSection: "Tuinafval: breng het naar een tuinkorf in de buurt, naar een Recyclepunt of anders bij het grof afval.",
+        wasteGuideGlasHowSection: "Hoe: In de container voor glas",
+        wasteGuideGlasWhereSection: "Waar: Kaart met containers in de buurt",
+        wasteGuideTextielHowSection: "Hoe: In de container voor textiel", wasteGuideTextielWhereSection: "Waar: Kaart met containers in de buurt", wasteGuideScreenTextielWasteText: "Hoe: In de container voor textiel Waar: Kaart met containers in de buurt",
+    }
+
+}
+
