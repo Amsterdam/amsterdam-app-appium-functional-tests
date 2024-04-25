882ef804e66a01cd4faf80ba04ecada560c80561
Monique van Benthem <m.van.benthem@amsterdam.nl>
2024-04-25 11:09:20 +0200

    edit locations

--- tests/step-definitions/location.steps.js
+++ tests/step-definitions/location.steps.js
@@ -8,9 +8,9 @@ import wasteGuideScreen from "../screenobjects/waste-guide.screen.js";
 
 const OS = driver.capabilities.platformName
 
-const locationsIOS = ['Mijn huidige locatie, In de buurt van Zeedijk 5', 'Mijn huidige locatie, In de buurt van Viergrenzenweg 97', 'Mijn huidige locatie, In de buurt van Vliehors 1', 'Mijn huidige locatie, In de buurt van "n Tip 1', 'Mijn huidige locatie, In de buurt van Ruhenbergerweg 35', 'Mijn huidige locatie, In de buurt van Nieuwe Statenzijl 23']
+const locationsArray = ['Mijn huidige locatie, In de buurt van Zeedijk 5', 'Mijn huidige locatie, In de buurt van Viergrenzenweg 97', 'Mijn huidige locatie, In de buurt van Vliehors 1', 'Mijn huidige locatie, In de buurt van "n Tip 1', 'Mijn huidige locatie, In de buurt van Ruhenbergerweg 35', 'Mijn huidige locatie, In de buurt van Nieuwe Statenzijl 23']
 const locationsAndroid = ['In de buurt van Zeedijk 5', 'In de buurt van Viergrenzenweg 97', 'In de buurt van Vliehors 1', 'In de buurt van "n Tip 1', 'In de buurt van Ruhenbergerweg 35', 'In de buurt van Nieuwe Statenzijl 23']
-//chai.expect(attr).to.be.oneOf(locationsIOS)
+//chai.expect(attr).to.be.oneOf(locationsArray)
 
 //When
 When(/ik geef mijn locatie door/, async () => {
@@ -32,13 +32,16 @@ Then(/mijn locatie wordt gebruikt voor het tonen van werkzaamheden/, async () =>
         await driver.pause(15000)
         const locationButtonLabel = await constructionWorkScreen.constructionWorkRequestLocationButton.getAttribute("label")
         console.log(await locationButtonLabel)
-        chai.expect(locationButtonLabel).to.be.oneOf(locationsIOS)
+        chai.expect(locationButtonLabel).to.be.oneOf(locationsArray)
+        //await expect(await constructionWorkScreen.constructionWorkRequestLocationButton).toHaveAttribute('label', 'Mijn huidige locatie, In de buurt van Zeedijk 5' || 'label', 'Mijn huidige locatie, In de buurt van Viergrenzenweg 97' || 'label', 'Mijn huidige locatie, In de buurt van Vliehors 1' || 'label', 'Mijn huidige locatie, In de buurt van "n Tip 1')
     }
     else {
-        await driver.pause(15000)
-        const locationText = await constructionWorkScreen.constructionWorkRequestLocationButtonText.getText()
+        await driver.pause(10000)
+        //const locationText = await constructionWorkScreen.constructionWorkRequestLocationButtonText.getText()
+        const locationText = await constructionWorkScreen.constructionWorkRequestLocationButton.getAttribute("content-desc")
         console.log(await locationText)
-        chai.expect(locationText).to.be.oneOf(locationsAndroid)
+        chai.expect(locationText).to.be.oneOf(locationsArray)
+        //chai.expect(locationText).to.be.oneOf(locationsAndroid)
     }
     await constructionWorkScreen.headerBackButton.click()
     await homeScreen.homeWasteGuideModuleButton.click()
@@ -48,13 +51,16 @@ Then(/mijn locatie wordt gebruikt voor het tonen van werkzaamheden/, async () =>
         await driver.pause(15000)
         const locationButtonLabel = await wasteGuideScreen.wasteGuideRequestLocationButton.getAttribute("label")
         console.log(await locationButtonLabel)
-        chai.expect(locationButtonLabel).to.be.oneOf(locationsIOS)
+        chai.expect(locationButtonLabel).to.be.oneOf(locationsArray)
+        //await expect(await wasteGuideScreen.wasteGuideRequestLocationButton).toHaveAttribute('label', 'Mijn huidige locatie, In de buurt van Zeedijk 5' || 'label', 'Mijn huidige locatie, In de buurt van Viergrenzenweg 97' || 'label', 'Mijn huidige locatie, In de buurt van Vliehors 1' || 'label', 'Mijn huidige locatie, In de buurt van "n Tip 1')
     }
     else {
-        await driver.pause(15000)
-        const locationText = await constructionWorkScreen.constructionWorkRequestLocationButtonText.getText()
+        await driver.pause(10000)
+        //const locationText = await wasteGuideScreen.wasteGuideRequestLocationButtonText.getText()
+        const locationText = await wasteGuideScreen.wasteGuideRequestLocationButton.getAttribute("content-desc")
         console.log(await locationText)
-        chai.expect(locationText).to.be.oneOf(locationsAndroid)
+        //chai.expect(locationText).to.be.oneOf(locationsAndroid)
+        chai.expect(locationText).to.be.oneOf(locationsArray)
     }
     await wasteGuideScreen.wasteGuideNotFoundMessage.waitForDisplayed(20000)
     await wasteGuideScreen.wasteGuideNotFoundMistakeButton.waitForDisplayed(20000)
