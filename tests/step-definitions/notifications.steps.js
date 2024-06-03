37e590c9b17d3e85563efdc7912557d568786751
Monique van Benthem <m.van.benthem@amsterdam.nl>
2024-06-03 16:10:29 +0200

    include all features

--- tests/step-definitions/notifications.steps.js
+++ tests/step-definitions/notifications.steps.js
@@ -52,6 +52,8 @@ Given(/ik ben OM\/CA en heb een plaats berichten module in de app/, async () => 
         await driver.executeScript('mobile: backgroundApp', [{ seconds: 3 }])
         await openDeepLinkUrl(url)
         await driver.pause(5000)
+        const contexts = await driver.getContexts()
+        console.log(contexts)
         await notificationsScreen.adwUsernameInput.waitForDisplayed(20000)
         await notificationsScreen.adwUsernameInput.click()
         await notificationsScreen.adwUsernameInput.addValue(adwUsername)
